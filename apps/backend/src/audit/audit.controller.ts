446d5d48d3dede50b75d89dbfd7bf13c1d46f678
osamamohti2002 <99078852@mydavinci.nl>
2026-06-07 16:31:29 +0200

    feat(audit-module): add AuditController with read-only endpoints

--- apps/backend/src/audit/audit.controller.ts
+++ apps/backend/src/audit/audit.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AuditService } from './audit.service';
-import { CreateAuditDto } from './dto/create-audit.dto';
-import { UpdateAuditDto } from './dto/update-audit.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'generated/prisma';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('audit')
 export class AuditController {
   constructor(private readonly auditService: AuditService) {}
 
+  @Roles(UserRole.ADMIN, UserRole.OPERATOR_HR, UserRole.OPERATOR_IT)
+  @Get()
+  findAll(@Query() pagination: PaginationDto){
+    return this.auditService.findAll(pagination);
+  }
+
+  @Roles(UserRole.ADMIN, UserRole.OPERATOR_HR, UserRole.OPERATOR_IT)
+  @Get(':id')
+  findById(@Param('id') id: string){
+    return this.auditService.findById(id);
+  }
 }
